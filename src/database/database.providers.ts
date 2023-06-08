63eae4c74d7e0639a770299da5b185d08c3a477c
alucarth <ltorrezs2008@gmail.com>
2023-06-08 11:08:18 -0400

    config

--- src/database/database.providers.ts
+++ src/database/database.providers.ts
@@ -1,16 +1,16 @@
 import { DataSource } from "typeorm";
-
+import 'dotenv/config'
 export const databaseProviders = [
     {
         provide: 'DATA_SOURCE',
         useFactory: async () => {
             const dataSource = new DataSource({
-                type: 'mysql',
-                host: 'localhost',
-                port: 3306,
-                username: 'root',
-                password: '123456',
-                database: 'loan_db',
+                type: 'postgres',
+                host: process.env.DB_HOST || 'localhost',
+                port: parseInt(process.env.DB_PORT) || 5432,
+                username: process.env.DB_USERNAME || 'postgres',
+                password: process.env.DB_PASSWORD || '',
+                database: process.env.DB_NAME || 'postgres',
                 entities: [
                     __dirname + '/../**/*.entity{.ts,.js}',
                 ],
