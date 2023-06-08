538521d8998e2e66733f6e5dd13d402bd163bd18
alucarth <ltorrezs2008@gmail.com>
2023-06-08 09:55:17 -0400

    user crud

---
+++ src/user/user.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+import { userProviders } from './user.providers';
+
+
+@Module({
+  imports:[DatabaseModule],
+  controllers: [UserController],
+  providers: [
+    ...userProviders,
+    UserService
+  ]
+})
+export class UserModule {}
