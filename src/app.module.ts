fd7797a5bd8ab543e416b15158539d7efc7f8651
alucarth <ltorrezs2008@gmail.com>
2023-06-11 17:18:00 -0400

    rol and user rol module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,6 @@ import { UserModule } from './user/user.module';
 import { AccountModule } from './modules/account/account.module';
 import { BranchModule } from './modules/branch/branch.module';
 import { PersonModule } from './modules/person/person.module';
-import { ResponseInterceptor } from './response.interceptor'
 import { CountryModule } from './modules/country/country.module';
 import { CityModule } from './modules/city/city.module';
 
@@ -13,6 +12,6 @@ import { CityModule } from './modules/city/city.module';
 @Module({
   imports: [UserModule,AccountModule, ],
   controllers: [AppController, ],
-  providers: [AppService,  ResponseInterceptor,],
+  providers: [AppService, ],
 })
 export class AppModule {}
