da9992c15929d9eb90888561a6db65e79d8b2b57
alucarth <ltorrezs2008@gmail.com>
2023-06-11 13:26:02 -0400

    branch db created

--- src/services/branch/branch.service.ts
+++ src/services/branch/branch.service.ts
@@ -1,4 +1,15 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { Branch } from 'src/modules/branch/branch.entity';
 
 @Injectable()
-export class BranchService {}
+export class BranchService {
+    constructor(
+        @Inject('BRANCH_REPOSITORY')
+        private branchRepository: Repository<Branch>
+    ){}
+
+    async findAll(): Promise<Branch[]>{
+        return this.branchRepository.find();
+    }
+}
