b13b6332d34a02bf89c67992c5f92c9076d34a4e
alucarth <ltorrezs2008@gmail.com>
2023-09-26 23:06:19 -0400

    reestructurando person_type

--- src/modules/person/person.module.ts
+++ src/modules/person/person.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { PersonController } from './person.controller';
 import { DatabaseModule } from 'src/database/database.module';
-import { personTypeProviders } from 'src/services/person_type/person_type.providers';
+
 import { PersonSeeder } from './person.seeder';
 import { accountProviders } from '../account/account.providers';
 import { personProviders } from './person.providers';
@@ -10,6 +10,7 @@ import { PersonService } from './person.service';
 import { AddressService } from '../address/address.service';
 import { cityProviders } from '../city/city.providers';
 import { countryProviders } from '../country/country.providers';
+import { personTypeProviders } from '../person_type/person_type.providers';
 
 @Module({
   imports: [DatabaseModule],
--- src/modules/person_type/person_type.seeder.ts
+++ src/modules/person_type/person_type.seeder.ts
@@ -1,40 +1,41 @@
-import { Inject, Injectable, OnModuleInit } from "@nestjs/common";
-import { PersonTypeService } from "src/services/person_type/person_type.service";
-import { PersonType } from "./person_type.entity";
-import { Repository } from "typeorm";
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import { PersonTypeService } from './person_type.service';
+import { PersonType } from './person_type.entity';
+import { Repository } from 'typeorm';
 
-  
 @Injectable()
 export class PersonTypeSeeder implements OnModuleInit {
-    constructor(
-        private readonly _personTypeService: PersonTypeService,
-        @Inject('PERSON_TYPE_REPOSITORY')
-        private personTypeRepository: Repository<PersonType>,
-        ) {}
+  constructor(
+    private readonly _personTypeService: PersonTypeService,
+    @Inject('PERSON_TYPE_REPOSITORY')
+    private personTypeRepository: Repository<PersonType>,
+  ) {}
 
-    async onModuleInit() {
-
+  async onModuleInit() {
     const exist = await this.personTypeRepository.find();
     if (exist.length > 0) {
-        console.log('Saltando proceso del seeder -> (Person_Type). Ya existen registros en la base de datos.');
-        return;
+      console.log(
+        'Saltando proceso del seeder -> (Person_Type). Ya existen registros en la base de datos.',
+      );
+      return;
     }
-    console.log('Iniciando el seeder -> (Person_Type). Cargando registros en la base de datos.');
+    console.log(
+      'Iniciando el seeder -> (Person_Type). Cargando registros en la base de datos.',
+    );
     const personTypeData = [
-        { name: 'Estudiante', state: true },
-        { name: 'Maestro', state: true },
-        { name: 'Ingeniero', state: true },
-        { name: 'Doctor', state: true },
-        { name: 'Artista', state: true },
-        ];
-    
-        for (const data of personTypeData) {
-            const person_type = new PersonType();
-            person_type.name = data.name;
-            person_type.state = data.state;
-    
-            await this._personTypeService.create(person_type);
-        }
+      { name: 'Estudiante', state: true },
+      { name: 'Maestro', state: true },
+      { name: 'Ingeniero', state: true },
+      { name: 'Doctor', state: true },
+      { name: 'Artista', state: true },
+    ];
+
+    for (const data of personTypeData) {
+      const person_type = new PersonType();
+      person_type.name = data.name;
+      person_type.state = data.state;
+
+      await this._personTypeService.create(person_type);
     }
+  }
 }
-  
