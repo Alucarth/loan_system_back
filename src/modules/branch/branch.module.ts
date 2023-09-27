ea8addffd89a7d17fcc64434e5a545a24297d72e
alucarth <ltorrezs2008@gmail.com>
2023-09-26 22:38:42 -0400

    reestructurando branch

--- src/modules/branch/branch.module.ts
+++ src/modules/branch/branch.module.ts
@@ -1,17 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from 'src/database/database.module';
-import { branchProviders } from 'src/services/branch/branch.providers';
-import { BranchService } from 'src/services/branch/branch.service';
-import { BranchController } from 'src/controllers/branch/branch.controller'; 
+import { branchProviders } from './branch.providers';
+import { BranchService } from './branch.service';
+import { BranchController } from './branch.controller';
 import { BranchSeeder } from './branch.seeder';
 
 @Module({
-    imports: [ DatabaseModule],
-    controllers: [BranchController],
-    providers: [
-        ...branchProviders,
-        BranchService,
-        BranchSeeder
-    ]
+  imports: [DatabaseModule],
+  controllers: [BranchController],
+  providers: [...branchProviders, BranchService, BranchSeeder],
 })
 export class BranchModule {}
--- src/modules/branch/branch.seeder.ts
+++ src/modules/branch/branch.seeder.ts
@@ -1,49 +1,52 @@
-import { Inject, Injectable, OnModuleInit } from "@nestjs/common";
-import { BranchService } from "src/services/branch/branch.service";
-import { Branch } from "./branch.entity";
-import { Repository } from "typeorm";
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+
+import { Branch } from './branch.entity';
+import { Repository } from 'typeorm';
+import { BranchService } from './branch.service';
 
 @Injectable()
 export class BranchSeeder implements OnModuleInit {
-    constructor(
-        private readonly _branchService: BranchService,
-        @Inject('BRANCH_REPOSITORY')
-        private branchRepository: Repository<Branch>,
-        ) {}
+  constructor(
+    private readonly _branchService: BranchService,
+    @Inject('BRANCH_REPOSITORY')
+    private branchRepository: Repository<Branch>,
+  ) {}
 
-    async onModuleInit() {
-
-        //await this.branchRepository.query('TRUNCATE TABLE branch RESTART IDENTITY CASCADE');
-        const exist = await this.branchRepository.find();
-        if (exist.length > 0) {
-            console.log('Saltando proceso del seeder -> (Branch). Ya existen registros en la base de datos.');
-            return;
-        }
-        console.log('Iniciando el seeder -> (Branch). Cargando registros en la base de datos.');
-        const branchData = [
-            {
-              name: 'Sucursal 1',
-              address: 'Dirección 1',
-              phone: '123456789',
-              //account: 1  /agregar esto en los dto por los cambios de david
-            },
-            {
-              name: 'Sucursal 2',
-              address: 'Dirección 2',
-              phone: '987654321',
-              //account: 2  /agregar esto en los dto por los cambios de david
-            },
-            // Agregar más objetos con datos de prueba
-          ];
-    
-        for (const data of branchData) {
-            const branch = new Branch();
-            branch.name = data.name;
-            branch.address = data.address;
-            branch.phone = data.phone;
-    
-            await this._branchService.create(branch);
-        }
+  async onModuleInit() {
+    //await this.branchRepository.query('TRUNCATE TABLE branch RESTART IDENTITY CASCADE');
+    const exist = await this.branchRepository.find();
+    if (exist.length > 0) {
+      console.log(
+        'Saltando proceso del seeder -> (Branch). Ya existen registros en la base de datos.',
+      );
+      return;
     }
+    console.log(
+      'Iniciando el seeder -> (Branch). Cargando registros en la base de datos.',
+    );
+    const branchData = [
+      {
+        name: 'Sucursal 1',
+        address: 'Dirección 1',
+        phone: '123456789',
+        //account: 1  /agregar esto en los dto por los cambios de david
+      },
+      {
+        name: 'Sucursal 2',
+        address: 'Dirección 2',
+        phone: '987654321',
+        //account: 2  /agregar esto en los dto por los cambios de david
+      },
+      // Agregar más objetos con datos de prueba
+    ];
+
+    for (const data of branchData) {
+      const branch = new Branch();
+      branch.name = data.name;
+      branch.address = data.address;
+      branch.phone = data.phone;
+
+      await this._branchService.create(branch);
+    }
+  }
 }
-  
