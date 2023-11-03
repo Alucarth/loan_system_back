cda0cca63a21acb8393e29c60ba305ac459fce66
alucarth <ltorrezs2008@gmail.com>
2023-11-03 00:32:11 -0400

    client module

--- src/modules/client/client.module.ts
+++ src/modules/client/client.module.ts
@@ -1,4 +1,20 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from 'src/database/database.module';
+import { ClientController } from './client.controller';
+import { clientProviders } from './client.provider';
+import { accountProviders } from '../account/account.providers';
+import { userProviders } from '../user/user.providers';
+import { ClientService } from './client.service';
+import { ClientTypeSeeder } from '../client_type/client_type.seeder';
 
-@Module({})
+@Module({
+  imports: [DatabaseModule],
+  controllers: [ClientController],
+  providers: [
+    ...clientProviders,
+    ...accountProviders,
+    ...userProviders,
+    ClientService,
+  ],
+})
 export class ClientModule {}
