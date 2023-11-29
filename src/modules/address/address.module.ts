7a56b74d233900c6f86d3b60e875ab248c35d476
alucarth <ltorrezs2008@gmail.com>
2023-11-28 21:53:56 -0400

    address bug fixes

--- src/modules/address/address.module.ts
+++ src/modules/address/address.module.ts
@@ -5,9 +5,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { City } from '../city/city.entity';
 import { Address } from './address.entity';
 import { Person } from '../person/person.entity';
+import { AddressSeeder } from './address.seeder';
 @Module({
   imports: [TypeOrmModule.forFeature([City, Address, Person])],
   controllers: [AddressController],
-  providers: [AddressService],
+  providers: [AddressService, AddressSeeder],
 })
 export class AddressModule {}
